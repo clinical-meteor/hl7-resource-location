40aa3e0b7a2792cd93fd3922cc14c1977362fecb
Abigail Watson <awatson1978@gmail.com>
2016-04-10 13:21:58 -0700

    circle.yml; tests; lib; package.js updates

--- server/rest.js
+++ server/rest.js
@@ -7,22 +7,112 @@ JsonRoutes.Middleware.use(
 
 
 
-JsonRoutes.add("get", "/location/:id", function (req, res, next) {
-  console.log('GET /location/' + req.params.id);
+JsonRoutes.add("get", "/fhir/Location/:id", function (req, res, next) {
+  process.env.DEBUG && console.log('GET /fhir/Location/' + req.params.id);
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
   var accessTokenStr = (req.params && req.params.access_token) || (req.query && req.query.access_token);
   var accessToken = oAuth2Server.collections.accessToken.findOne({accessToken: accessTokenStr});
 
-  if (accessToken) {
-    console.log('accessToken', accessToken);
-    console.log('accessToken.userId', accessToken.userId);
+  if (accessToken || process.env.NOAUTH) {
+    process.env.TRACE && console.log('accessToken', accessToken);
+    process.env.TRACE && console.log('accessToken.userId', accessToken.userId);
+
+    if (typeof SiteStatistics === "object") {
+      SiteStatistics.update({_id: "configuration"}, {$inc:{
+        "Locations.count.read": 1
+      }});
+    }
 
     var id = req.params.id;
-    console.log('Locations.findOne(id)', Locations.findOne(id));
+    var locationData = Locations.findOne(id);
+    delete locationData._document;
+    process.env.TRACE && console.log('locationData', locationData);
 
     JsonRoutes.sendResult(res, {
       code: 200,
-      data: Locations.findOne(id)
+      data: locationData
+    });
+  } else {
+    JsonRoutes.sendResult(res, {
+      code: 401
+    });
+  }
+});
+
+
+
+JsonRoutes.add("get", "/fhir/Location", function (req, res, next) {
+  process.env.DEBUG && console.log('GET /fhir/Location', req.query);
+  // console.log('GET /fhir/Location', req.query);
+  // console.log('process.env.DEBUG', process.env.DEBUG);
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  var accessTokenStr = (req.params && req.params.access_token) || (req.query && req.query.access_token);
+  var accessToken = oAuth2Server.collections.accessToken.findOne({accessToken: accessTokenStr});
+
+  if (accessToken || process.env.NOAUTH) {
+    process.env.TRACE && console.log('accessToken', accessToken);
+    process.env.TRACE && console.log('accessToken.userId', accessToken.userId);
+
+    if (typeof SiteStatistics === "object") {
+      SiteStatistics.update({_id: "configuration"}, {$inc:{
+        "Locations.count.search-type": 1
+      }});
+    }
+
+    var databaseQuery = {};
+
+    // // TODO:  Replace with Appointment specificy query parameters
+    //
+    // if (req.query.family) {
+    //   databaseQuery['name'] = {
+    //     $elemMatch: {
+    //       'family': req.query.family
+    //     }
+    //   };
+    // }
+    // if (req.query.given) {
+    //   databaseQuery['name'] = {
+    //     $elemMatch: {
+    //       'given': req.query.given
+    //     }
+    //   };
+    // }
+    // if (req.query.name) {
+    //   databaseQuery['name'] = {
+    //     $elemMatch: {
+    //       'text': req.query.text
+    //     }
+    //   };
+    // }
+    // if (req.query.identifier) {
+    //   databaseQuery['_id'] = req.query.identifier;
+    // }
+    // if (req.query.gender) {
+    //   databaseQuery['gender'] = req.query.gender;
+    // }
+    // if (req.query.birthdate) {
+    //   databaseQuery['birthDate'] = req.query.birthdate;
+    // }
+
+    process.env.DEBUG && console.log('databaseQuery', databaseQuery);
+    process.env.DEBUG && console.log('Locations.find(id)', Locations.find(databaseQuery).fetch());
+
+    // because we're using BaseModel and a _transform() function
+    // Locations returns an object instead of a pure JSON document
+    // it stores a shadow reference of the original doc, which we're removing here
+    var locationData = Locations.find(databaseQuery).fetch();
+
+    locationData.forEach(function(patient){
+      delete patient._document;
+    });
+
+    JsonRoutes.sendResult(res, {
+      code: 200,
+      data: locationData
     });
   } else {
     JsonRoutes.sendResult(res, {
